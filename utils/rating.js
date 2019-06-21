4872c25fef789c8b635051f78fecb6643df71fb7
bami55 <1992bami@gmail.com>
2019-06-21 14:03:22 +0900

    レーティング処理　初期

---
+++ utils/rating.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const match_config = require(__dirname + '/../config/match.json');
+const db = require(__dirname + '/../database/models/index.js');
+
+exports.updateRating = async (matchId) => { }
