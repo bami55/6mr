5b941577aaf20b92c78aaae8601c3f89feefaf0b
bami <1992bami@gmail.com>
2019-06-22 23:07:09 +0900

    リファクタリング

--- utils/rating.js
+++ utils/rating.js
@@ -4,6 +4,7 @@ const match_config = require(__dirname + '/../config/match.json');
 const db = require(__dirname + '/../database/models/index.js');
 
 exports.updateRating = async (guild, matchId) => {
+  // 試合結果取得
   const result = await db.match_results.findOne({
     where: {
       match_id: matchId
@@ -11,6 +12,7 @@ exports.updateRating = async (guild, matchId) => {
   });
   if (!result) return;
 
+  // エントリー全ユーザー取得
   const users = await db.match_users.findAll({
     where: {
       match_id: matchId
@@ -18,11 +20,15 @@ exports.updateRating = async (guild, matchId) => {
   });
   if (!users) return;
 
-  // ユーザー更新（Rate計算、Tier昇格・降格、役職付与）
+  // 報告用ユーザー取得
   let reportUsers = [];
   for (let i = 0; i < users.length; i++) {
-    reportUsers.push(await updateUser(guild, result, users[i]));
+    // ユーザー更新（Rate計算、Tier昇格・降格、役職付与）
+    let reportUser = await updateUser(guild, result, users[i]);
+    reportUsers.push(reportUser);
   }
+
+  // TODO: 特定のチャンネルに試合結果、レート変動を出力する
 }
 
 /**
@@ -44,35 +50,78 @@ async function updateUser(guild, result, user) {
   reportResult.name = guildMember.displayName;
   
   let updUser = Object.assign({}, userInfo);
+  let isWin = result.win_team === user.team;
+
   if (userInfo) {
     // レート計算
-    if (result.win_team === user.team) {
-      updUser.rate += match_config.rate.win;
-      updUser.win += 1;
-      updUser.streak += 1;
-      reportResult.rate = `${updUser.rate} (+${match_config.rate.win})`;
-    } else {
-      updUser.rate -= match_config.rate.lose;
-      updUser.lose += 1;
-      updUser.streak = 0;
-      reportResult.rate = `${updUser.rate} (-${match_config.rate.lose})`;
-    }
+    updUser = calcRating(isWin, updUser);
 
     // Tierチェンジ
-    const oldTier = getTierByRate(userInfo.rate);
-    const newTier = getTierByRate(updUser.rate);
-    const oldTierRole = await guild.roles.get(oldTier);
-    const newTierRole = await guild.roles.get(newTier);
-    if (newTier.tier !== oldTier.tier) {
-      updUser.tier = newTier.tier;
-      await guildMember.removeRole(oldTier.role_id);
-      await guildMember.addRole(newTier.role_id);
-      reportResult.tier = `${oldTierRole.name} -> ${newTierRole.name}`;
+    let resultChangeTier = await changeTier(guildMember, userInfo, updUser, reportResult);
+    updUser = resultChangeTier.updUser;
+    reportResult = resultChangeTier.reportResult;
+    
+    // レート変動出力
+    if (isWin) {
+      reportResult.rate = `${updUser.rate} (+${match_config.rate.win})`;
     } else {
-      reportResult.tier = oldTierRole.name;
+      reportResult.rate = `${updUser.rate} (-${match_config.rate.lose})`;
     }
   }
+
+  // 結果出力用データ返却
   return reportResult;
+}
+
+/**
+ * レート計算
+ * @param {*} isWin 
+ * @param {*} updUser 
+ */
+function calcRating(isWin, updUser) {
+  if (isWin) {
+    updUser.rate += match_config.rate.win;
+    updUser.win += 1;
+    updUser.streak += 1;
+  } else {
+    updUser.rate -= match_config.rate.lose;
+    updUser.lose += 1;
+    updUser.streak = 0;
+  }
+}
+
+/**
+ * Tierチェンジ（Tier昇格・降格、役職設定）
+ * @param {*} guildMember 
+ * @param {*} userInfo 
+ * @param {*} updUser 
+ * @param {*} reportResult 
+ */
+async function changeTier(guildMember, userInfo, updUser, reportResult) {
+  // Tierデータ取得
+  const oldTier = getTierByRate(userInfo.rate);
+  const newTier = getTierByRate(updUser.rate);
+
+  // Tier役職取得
+  const oldTierRole = await guildMember.guild.roles.get(oldTier);
+  const newTierRole = await guildMember.guild.roles.get(newTier);
+
+  if (newTier.tier !== oldTier.tier) {
+    // Tier変更
+    updUser.tier = newTier.tier;
+    // TODO: 降格したときに中央値にするかどうかは確認中
+    updUser.rate = newTier.median_rate;
+    await guildMember.removeRole(oldTier.role_id);
+    await guildMember.addRole(newTier.role_id);
+    reportResult.tier = `${oldTierRole.name} -> ${newTierRole.name}`;
+  } else {
+    // Tier維持
+    reportResult.tier = oldTierRole.name;
+  }
+  return {
+    updUser: updUser,
+    reportResult: reportResult
+  };
 }
 
 /**
